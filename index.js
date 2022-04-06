770cb76d013a7f2d1eaa9874e0376942b7faa5b9
Alexi543 <alexilord2008@gmail.com>
2022-04-06 19:21:49 +0000

    15:21/06/04/22

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ import { random } from "./JavaScript-Tools/random.js";
 const BUTTONS = document.querySelectorAll(".btn");
 const DIVIDERS_OUTPUT = document.getElementById("output-dividers");
 const DIVIDERS_NUMBER = document.getElementById("dividers-num-value");
-const COMMUN_DIV_OUTPUT = document.getElementById("output-commun-dividers");
-const COMMUN_DIV_NUMBER_1 = document.getElementById("commun-dividers-num-1");
-const COMMUN_DIV_NUMBER_2 = document.getElementById("commun-dividers-num-2");
 
-const clearInputToNumber = (input) => string.keepCharacters(input, string.digits);
+const clearInputToNumberGood = (input) => string.keepCharacters(input, string.digits);
+
+const clearInputToNumberBad = (input) => {
+    switch (random.randrange(1, 3)) {
+        case 1:
+            return string.keepCharacters(input, string.digits);
+        case 2:
+            return random.randrange(1, 100);
+        case 3:
+            return "Error";
+    }
+}
 
 const foundDividers = (number) => {
     let dividers = [];
@@ -18,47 +26,21 @@ const foundDividers = (number) => {
     return dividers;
 }
 
-const foundSameDividers = (divider1, divider2) => {
-    let dividersList1 = foundDividers(divider1);
-    let dividersList2 = foundDividers(divider2);
-    
-    return dividersList1.filter(x => dividersList2.indexOf(x) !== -1);
-}
-
 BUTTONS.forEach(button => {
     button.addEventListener("click", event => {
         if (button.id === "start-dividers") {
             let number = "";
             let out;
 
-            if (DIVIDERS_NUMBER.value.toString().split("").length > 0) number  = clearInputToNumber(DIVIDERS_NUMBER.value);
+            if (DIVIDERS_NUMBER.value.toString().split("").length > 0) number  = clearInputToNumberBad(DIVIDERS_NUMBER.value);
             else number = "Error";
 
-            if (number.split("").length <= 0) number = "Error";
+            if (number.toString().split("").length <= 0) number = "Error";
 
             if (number !== "Error") out = foundDividers(number).toString();
             else out = "Error";
 
             DIVIDERS_OUTPUT.innerHTML = `Number = ${number} <br> ${out}`;
-        } else if (button.id === "start-commun-dividers") {
-            let number1 = "";
-            let number2 = "";
-            let out;
-
-            if (COMMUN_DIV_NUMBER_1.value.toString().split("").length > 0) number1 = clearInputToNumber(COMMUN_DIV_NUMBER_1.value);
-            else number1 = "Error";
-
-            if (number1.split("").length <= 0) number1 = "Error";
-
-            if (COMMUN_DIV_NUMBER_2.value.toString().split("").length > 0) number2 = clearInputToNumber(COMMUN_DIV_NUMBER_2.value);
-            else number2 = "Error";
-
-            if (number2.split("").length <= 0) number2 = "Error";
-
-            if (number1 !== "Error" && number2 !== "Error") out = foundSameDividers(number1, number2).toString();
-            else out = "Error";
-
-            COMMUN_DIV_OUTPUT.innerHTML = `Number 1 = ${number1} <br> Number 2 = ${number2} <br> ${out}`;
         }
     })
 })
